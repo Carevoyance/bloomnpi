1cc722dc4b44cd651855b3e244399aa9b0d354ea
Daniel Imfeld <daniel@danielimfeld.com>
2014-12-13 21:22:30 -0600

    Remove tables that aren't needed anymore. Add index on npi,revision

--- sql/index.sql
+++ sql/index.sql
@@ -1,10 +1,2 @@
 CREATE INDEX ON bloom.npis (npi);
-
-CREATE INDEX ON bloom.npi_licenses (npi_id);
-CREATE INDEX ON bloom.npis (business_location_id);
-CREATE INDEX ON bloom.npis (practice_location_id);
-CREATE INDEX ON bloom.npis (organization_official_id);
-CREATE INDEX ON bloom.npi_other_identifiers (npi_id);
-CREATE INDEX ON bloom.npis (parent_orgs_id);
-CREATE INDEX ON bloom.npi_taxonomy_groups (npi_id);
-CREATE INDEX ON bloom.npis (file_id);
+CREATE UNIQUE INDEX npi_and_revision ON bloom.npis(npi,revision);
